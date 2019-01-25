20e6db810029b3935672c13dfba853b6e2e056fb
Rinat R <evolticdev@gmail.com>
2019-01-25 16:05:22 +0500

    Add "less" and "import" dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -19,11 +19,13 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
+    `gatsby-plugin-less`,
+    'gatsby-plugin-root-import'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
+  ]
 }
