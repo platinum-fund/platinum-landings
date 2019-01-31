09616c0287868f7a9b5028df1ce27c149af615b0
Rinat R <evolticdev@gmail.com>
2019-01-31 17:55:59 +0500

    Add media block to preface section

--- src/components/sections/preface/index.js
+++ src/components/sections/preface/index.js
@@ -19,6 +19,89 @@ const PrefacePure = () => (
           Get consultation
         </button>
       </div>
+      <div className="card _theme_media">
+        <a
+          className="card__item"
+          href="http://markets.financialcontent.com/townhall/news/read/37494272"
+          target="_blank"
+        >
+          <img
+            className="image"
+            src={require('src/images/media/financial.png')}
+          />
+        </a>
+        <a
+          className="card__item"
+          href="https://seekingalpha.com/instablog/49582419-alena-kalionova/5251739-roller-coaster-rise-cryptocurrencies-2018-experts-explain"
+          target="_blank"
+        >
+          <img className="image" src={require('src/images/media/alpha.png')} />
+        </a>
+        <a
+          className="card__item"
+          href="https://hackernoon.com/security-tokens-in-the-us-regulations-and-exemptions-under-the-sec-laws-4640b5570948"
+          target="_blank"
+        >
+          <img className="image" src={require('src/images/media/medium.png')} />
+        </a>
+        <a
+          className="card__item"
+          href="https://dailyhodl.com/2018/12/25/legal-aspects-of-sto-in-switzerland-how-to-issue-security-tokens-in-crypto-valley/"
+          target="_blank"
+        >
+          <img
+            className="image"
+            src={require('src/images/media/the-daily-hodl.png')}
+          />
+        </a>
+        <a
+          className="card__item"
+          href="https://www.tokendesk.io/the-roller-coaster-rise-of-cryptocurrencies-in-2018-experts-explain/"
+          target="_blank"
+        >
+          <img
+            className="image"
+            src={require('src/images/media/tokendesk.png')}
+          />
+        </a>
+        <a
+          className="card__item"
+          href="https://www.coinspeaker.com/2019-crypto-regulation-stos/"
+          target="_blank"
+        >
+          <img
+            className="image"
+            src={require('src/images/media/coinspeaker.png')}
+          />
+        </a>
+        <a
+          className="card__item"
+          href="https://hellyellie.quora.com/Security-tokens-in-the-US-regulations-and-exemptions-under-the-SEC-laws"
+          target="_blank"
+        >
+          <img className="image" src={require('src/images/media/quora.png')} />
+        </a>
+        <a
+          className="card__item"
+          href="https://www.livebitcoinnews.com/are-stos-the-step-in-the-right-direction-for-crypto/"
+          target="_blank"
+        >
+          <img
+            className="image"
+            src={require('src/images/media/bitcoin-news.png')}
+          />
+        </a>
+        <a
+          className="card__item"
+          href="http://blockchaintoday.co.kr/news/viewpage.php?wr_id=1651//blockchaintoday.co.kr/news/viewpage.php?wr_id=1651"
+          target="_blank"
+        >
+          <img
+            className="image"
+            src={require('src/images/media/blockhain-today.png')}
+          />
+        </a>
+      </div>
       <div className="scroll-down">
         <p className="scroll-down__text">scroll to know more</p>
         <img
