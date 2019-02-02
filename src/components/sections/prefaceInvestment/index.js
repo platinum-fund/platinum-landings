4b001338e59e1de35cddca330dd45c2b6c94cbd8
Rinat R <evolticdev@gmail.com>
2019-02-02 13:08:21 +0500

    Change className of header in the preface section

--- src/components/sections/prefaceInvestment/index.js
+++ src/components/sections/prefaceInvestment/index.js
@@ -5,13 +5,13 @@ import ContactForm from 'src/components/contactForm'
 const prefaceInvestment = () => (
   <section className="section_preface-investment">
     <div className="wrapper_shrink">
-      <div className="headline">
-        <h6 className="title title_type_main-attention">
+      <div className="header">
+        <h2 className="title title_type_main-attention">
           Invest in the working ICO/STO
-        </h6>
-        <h1 className="title title_type_main">
+        </h2>
+        <h2 className="title title_type_main">
           consulting and management business
-        </h1>
+        </h2>
         <h4 className="title title_type_subtitle">
           One of the biggest and profitable companies on the crypto market
         </h4>
