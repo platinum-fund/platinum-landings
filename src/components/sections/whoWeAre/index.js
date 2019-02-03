2f417f5b5264e239919093a872fb42879b7a7326
Rinat R <evolticdev@gmail.com>
2019-02-03 19:37:54 +0500

    Add styles and content to who we are section

--- src/components/sections/whoWeAre/index.js
+++ src/components/sections/whoWeAre/index.js
@@ -1,25 +1,46 @@
 import React from 'react'
 import './section_who-we-are.less'
+import Slider from 'src/components/slider'
+import SliderItem from 'src/components/slider/SliderItemPure'
+
+const SliderItemTitle = ({ title }) => (
+  <div>
+    <p className="slider__subtitle">Facts:</p>
+    <h6 className="slider__title">{title}</h6>
+  </div>
+)
 
 const WhoWeAre = () => (
-  <section className="section_who-we-are">
-    <div className="wrapper_shrink">
-      <div>Slider</div>
-      <div className="headline">
+  <section className="section section_who-we-are">
+    <div className="wrapper_section-content">
+      <Slider>
+        <SliderItem
+          title={
+            <SliderItemTitle title="3+ years experience in crypto markets" />
+          }
+        />
+        <SliderItem
+          title={
+            <SliderItemTitle title="7+ years experience in financial markets" />
+          }
+        />
+        <SliderItem title={<SliderItemTitle title="112+ specialists" />} />
+      </Slider>
+      <header className="headline">
         <h2 className="title title_type_section-big-attention">Who we are:</h2>
         <h6 className="title title_type_subtitle">
           Platinum.fund is an international STO/IEO/ICO/POST ICO consulting,
           promotion and fundraising company with offices in offices in Japan,
-          Korea, Thailand, Belarus. Over 112 specialists with 3 years experience
-          in crypto and 7 years in financial markets. We have an extensive range
-          of services and expertise in marketing and PR, management, consulting,
-          programming, cyber-security, education, legal advisory and many more.
+          Korea, Thailand, Belarus. <br /> Over 112 specialists with 3 years
+          experience in crypto and 7 years in financial markets. <br /> We have
+          an extensive range of services and expertise in marketing and PR,
+          management, consulting, programming, cyber-security, education, legal
+          advisory and many more.
         </h6>
-      </div>
+      </header>
       <button className="button button_theme_light">
         Play video about Platinum
       </button>
-      <div>Socials</div>
     </div>
   </section>
 )
