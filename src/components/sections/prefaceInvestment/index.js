2f22c9adac7930920a95a9affbf490bbb7ca86a2
Rinat R <evolticdev@gmail.com>
2019-02-03 19:36:37 +0500

    Add styles and content to business page preface

--- src/components/sections/prefaceInvestment/index.js
+++ src/components/sections/prefaceInvestment/index.js
@@ -1,23 +1,54 @@
 import React from 'react'
 import './section_preface-investment.less'
 import ContactForm from 'src/components/contactForm'
+import Slider from 'src/components/slider'
+import SliderItem from 'src/components/slider/SliderItemPure'
 
 const prefaceInvestment = () => (
-  <section className="section_preface-investment">
-    <div className="wrapper_shrink">
-      <div className="header">
-        <h2 className="title title_type_main-attention">
-          Invest in the working ICO/STO
-        </h2>
-        <h2 className="title title_type_main">
-          consulting and management business
-        </h2>
-        <h4 className="title title_type_subtitle">
-          One of the biggest and profitable companies on the crypto market
-        </h4>
+  <section className="section section_preface-investment">
+    <div className="wrapper_section-content">
+      <div className="column_left">
+        <header className="header">
+          <h2 className="headline">
+            <span className="title title_type_main-attention">
+              Invest in the working ICO/STO
+            </span>
+            <span className="title title_type_main">
+              consulting and management business
+            </span>
+          </h2>
+          <h4 className="title title_type_subtitle">
+            One of the biggest and profitable companies on the crypto market
+          </h4>
+        </header>
+        <ContactForm
+          label="Enter your email to get the prospectus"
+          placeholder="Your email*"
+          buttonText="Get information"
+        />
       </div>
-      <ContactForm />
-      <div>Slider</div>
+      <div className="column_right">
+        <Slider>
+          <SliderItem title={'ceo'}>
+            <img
+              className="slider-item__background"
+              src={require('src/images/section-preface-investment/team/angelika.png')}
+            />
+          </SliderItem>
+          <SliderItem title={'example'}>
+            <img
+              className="slider-item__background"
+              src={require('src/images/section-preface-investment/team/alex.png')}
+            />
+          </SliderItem>
+          <SliderItem title={'example2'}>
+            <img
+              className="slider-item__background"
+              src={require('src/images/section-preface-investment/team/dmitry.png')}
+            />
+          </SliderItem>
+        </Slider>
+      </div>
     </div>
   </section>
 )
