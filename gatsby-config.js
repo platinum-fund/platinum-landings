34515753b5e1c60b29b607c6e140b0c2d28a46a6
Rinat R <evolticdev@gmail.com>
2019-02-05 17:14:11 +0500

    Change theme color of the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#ff6932`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png` // This path is relative to the root of the site.
       }
