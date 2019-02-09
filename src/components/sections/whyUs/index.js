ad8cb798e32b74b9ac85ee738087d87529d9a46c
NIkita <nik.pisarskiy@gmail.com>
2019-02-09 09:51:40 +0200

    Fixed: new slides, fixes #511

--- src/components/sections/whyUs/index.js
+++ src/components/sections/whyUs/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import './section_why-us.less'
 import Slider from 'src/components/slider'
 import SliderItem from 'src/components/slider/SliderItemPure'
+import slidesContent from './slider/content'
 
-const SliderItemTitle = ({ className = '' }) => (
+const SliderItemTitle1 = ({ className = '' }) => (
   <span className={`slider__title ${className}`}>
     <span className="slider__title-attention">700+</span>
     Projects Consulted and
@@ -21,12 +22,26 @@ const WhyUs = () => (
         <span className="title title_type_section-big">?</span>
       </h2>
       <Slider>
-        <SliderItem title={<SliderItemTitle />}>
-          <img
-            className="slider-item__background"
-            src={require('src/images/section-why-us/slide-1.png')}
-          />
-        </SliderItem>
+        {slidesContent.map((slideItem, i) => (
+          <SliderItem
+            title={slideItem.titles.map((titleItem, ia) => (
+              <span
+                className={
+                  titleItem.attentionText ? 'slider__title-attention' : ''
+                }
+                key={`whyUsTitle${ia}`}
+              >
+                {titleItem.text}
+              </span>
+            ))}
+            key={`whyUsSlide${i}`}
+          >
+            <img
+              className="slider-item__background"
+              src={require('src/images/section-why-us/slide-1.png')}
+            />
+          </SliderItem>
+        ))}
       </Slider>
     </div>
   </section>
