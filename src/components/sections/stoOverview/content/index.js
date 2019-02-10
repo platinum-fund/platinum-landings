944d233bc6154deb0c1d22b2b4f056e466f00807
Katja Ezerskaya <ezersk@gmail.com>
2019-02-10 16:51:49 +0300

    fix section StoOverview

--- src/components/sections/stoOverview/content/index.js
+++ src/components/sections/stoOverview/content/index.js
@@ -1,11 +1,14 @@
 const contain = {
   title_black: 'What is',
-  title_orange: ' Security Token Offering?',
-  image: require('src/images/section-sto-overview/coin.svg'),
+  title_orange: ' Security Token Offering',
+  title_black2: '?',
   describe:
     'A Security Token Offering (STO) is the cryptographic tokenization of your capital providing investors with a digital share of your company.',
-  describe2:
-    'Security Tokens are defined as financial securities in accordance with the Howey test and empower investors with financial benefits such as:',
+  describe2: ' are defined as',
+  describe3:
+    'in accordance with the Howey test and empower investors with financial benefits such as:',
+  describe_word: 'Security Tokens',
+  describe_word2: ' financial securities ',
   list1: 'DIVIDENDS',
   list2: 'EQUITY',
   list3: 'PROFIT SHARING RIGHTS',
--- src/components/sections/stoOverview/index.js
+++ src/components/sections/stoOverview/index.js
@@ -3,25 +3,33 @@ import './sto-overview.less'
 
 const StoOverview = ({ contain }) => (
   <section className="section section_sto-overview">
-    <div className="wrapper_shrink">
-      <div className="wrapper_content">
+    <div className="wrapper _shrink">
+      <div className="wrapper _content">
         <h2 className="title title_type_section">
           {contain.title_black}
-          <span className="title title_type_section _orange">
+          <span className="title title_section _orange">
             {contain.title_orange}
           </span>
+          {contain.title_black2}
         </h2>
-        <p className="text fz-15 _light-gray">{contain.describe}</p>
-        <p className="text fz-15 _light-gray">{contain.describe2}</p>
-        <ul className="list _theme_diamond">
-          <li className="text fz-20 _dark-gray">{contain.list1}</li>
-          <li className="text fz-20 _dark-gray">{contain.list2}</li>
-          <li className="text fz-20 _dark-gray">{contain.list3}</li>
-          <li className="text fz-20 _dark-gray">{contain.list4}</li>
+        <p className="section_description">{contain.describe}</p>
+        <p className="section_description">
+          <span className="section_description--bold">
+            {contain.describe_word}
+          </span>
+          {contain.describe2}
+          <span className="section_description--bold">
+            {' '}
+            {contain.describe_word2}{' '}
+          </span>
+          {contain.describe3}
+        </p>
+        <ul className="section__list theme_diamond">
+          <li className="section__text">{contain.list1}</li>
+          <li className="section__text">{contain.list2}</li>
+          <li className="section__text">{contain.list3}</li>
+          <li className="section__text">{contain.list4}</li>
         </ul>
-      </div>
-      <div className="wrapper_image">
-        <img className="article__header-image" src={contain.image} />
       </div>
     </div>
   </section>
