e88f33e445866e680800e93a2d2ba7550918d397
Rinat R <evolticdev@gmail.com>
2019-02-18 11:32:29 +0500

    Delete empty link

--- src/components/sections/prefaceNav/index.js
+++ src/components/sections/prefaceNav/index.js
@@ -3,18 +3,21 @@ import './section_preface-nav.less'
 import 'src/common.blocks/social/social-links.less'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-
 const PrefaceNav = () => (
   <section className="section_preface-nav">
     <div className="wrapper_shrink">
       <div className="headline">
         <div className="headline _title">
-          <h1 className="title title_type_main">FUNDRAISING
-          <span className="title title_type_main _white">FOR ENTERPRISES AND
-            VENTURES THROUGH SECURITY TOKEN OFFERING</span>
-          <span className="title title_type_main">STO</span>
+          <h1 className="title title_type_main">
+            FUNDRAISING
+            <span className="title title_type_main _white">
+              FOR ENTERPRISES AND VENTURES THROUGH SECURITY TOKEN OFFERING
+            </span>
+            <span className="title title_type_main">STO</span>
           </h1>
-          <h4 className="title title_type_second">Get your project reviewed now!</h4>
+          <h4 className="title title_type_second">
+            Get your project reviewed now!
+          </h4>
         </div>
         <div className="wrapper_buttons">
           <button className="js-form-button button button_theme_main">
@@ -66,17 +69,6 @@ const PrefaceNav = () => (
               <img
                 className="service__img"
                 src={require('src/images/icons/social/twitter.svg')}
-              />
-            </a>
-            <a
-              className="service"
-              href="#"
-              rel="nofollow noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                className="service__img"
-                src={require('src/images/icons/social/line.svg')}
               />
             </a>
           </div>
